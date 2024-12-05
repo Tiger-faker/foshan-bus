86b3f4ce3e69c214ab7f67cb528a5a0042fe4560
Tiger-faker <2297918261@qq.com>
2024-12-05 17:19:58 +0800

    new file:   .vscode/launch.json 	modified:   pages.json 	modified:   pages/index/index.vue 	new file:   pages/login/login.vue 	modified:   pages/user/user.vue

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -67,7 +67,7 @@ import { ref } from 'vue';
 <style lang="scss" scoped>
 	.background{
 		width: 100%;
-		height: 1400rpx;
+		height: 90vh;
 		background: linear-gradient(to bottom, #ff9767, #fdf6f6 800rpx);
 		padding-top: 50rpx;
 		.top{
---
+++ pages/login/login.vue
@@ -0,0 +1,18 @@
+<template>
+	<view class="background">
+		
+	</view>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+	.background{
+		width: 100%;
+		height: 1400rpx;
+		background: linear-gradient(to bottom, #ff9767, #fdf6f6 800rpx);
+		padding-top: 50rpx;
+	}
+</style>
